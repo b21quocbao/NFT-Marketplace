538040ab945e8f68998a1c199832698c7a455b17
Bao Ngo <ngobao2001@gmail.com>
2022-05-26 23:57:43 +0700

    fix: must reload after login

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
+import { useRouter } from 'next/router';
 
 function getLibrary(provider: any) {
   const library = new Web3Provider(provider);
@@ -11,9 +12,11 @@ function getLibrary(provider: any) {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
-      <Layout>
+      <Layout key={router.asPath}>
         <Component {...pageProps} />
       </Layout>
     </Web3ReactProvider>
