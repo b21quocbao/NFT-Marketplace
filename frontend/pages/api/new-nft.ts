5c53a1a2a02c65feb0aebda84b3bfec5e688a8cb
Bao Ngo <ngobao2001@gmail.com>
2022-05-28 22:35:59 +0700

    feat: create multiple nfts

--- frontend/pages/api/new-nft.ts
+++ frontend/pages/api/new-nft.ts
@@ -9,11 +9,34 @@ async function handler(req: any, res: any) {
 
     const nftsCollection = db.collection("nfts");
 
-    const result = await nftsCollection.insertOne(data);
+    for (let idx = 0; idx < data.assets.length; ++idx) {
+      const imageUrl = data.imageUrls[idx];
+      const assetURI = data.assetURIs[idx];
+      const metadataURI = data.metadataURIs[idx];
+      const { name, description } = data.assets[idx];
+      const collectionId = data.collectionId;
+      const chain = data.chain;
+      const status = data.status;
+      const tokenId = data.tokenId;
+      const userId = data.userId;
+
+      await nftsCollection.insertOne({
+        imageUrl,
+        assetURI,
+        metadataURI,
+        collectionId,
+        chain,
+        status,
+        tokenId,
+        userId,
+        name,
+        description
+      })
+    }
 
     client.close();
 
-    res.status(201).json({ id: result.insertedId.toString() });
+    res.status(201).json({ message: "Collection inserted!" });
   }
 }
 
